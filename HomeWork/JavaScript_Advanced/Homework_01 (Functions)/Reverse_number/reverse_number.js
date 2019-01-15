9cd40769a8d4ee979edbe2d25c1e9763d4cf16a5
Dejan Monev <kojkradepepsi@gmail.com>
2019-01-15 07:26:22 +0100

    Added JS Advanced (unfinished)

---
+++ HomeWork/JavaScript_Advanced/Homework_01 (Functions)/Reverse_number/reverse_number.js
@@ -0,0 +1,8 @@
+$("button").click(reverseNumber);
+
+function reverseNumber()
+{
+    let str = $("input").val().split("").reverse();
+    let number = Number(str.join(""));
+    $("<div>").text(`Reverse Number: ${number}`).appendTo("body");
+}
