6f715940dcf6dab2701de638d26f6156433c66c5
Dejan Monev <kojkradepepsi@gmail.com>
2019-02-02 16:50:24 +0100

    Star wars update

--- HomeWork/JavaScript_Advanced/Homework_04 (Star wars api)/script.js
+++ HomeWork/JavaScript_Advanced/Homework_04 (Star wars api)/script.js
@@ -1,191 +1,195 @@
-(() => {
-  const peopleUrl = 'https://swapi.co/api/people/';
-  const planetsUrl = 'https://swapi.co/api/planets/';
-  const people = [];
-  const planets = [];
+// (() => {
+const peopleUrl = 'https://swapi.co/api/people/';
+const planetsUrl = 'https://swapi.co/api/planets/';
+const people = [];
+const planets = [];
 
-  function writePeopleInTable(someArray) {
-    someArray.forEach(pageOfpeople => {
-      let listOfpeople = pageOfpeople.results;
-      listOfpeople.forEach(person => {
-        insertRowContentForPerson(person);
-      })
-    });
+function writePeopleInTable(list) {
+  list.forEach(person => {
+    insertRowContentForPerson(person);
+  })
+}
+
+function insertRowContentForPerson(somePerson) {
+
+  let row = tBodyPeople.insertRow();
+
+  let name = row.insertCell();
+  name.textContent = somePerson.name;
+
+  let gender = row.insertCell();
+  gender.textContent = somePerson.gender;
+
+  let birthYear = row.insertCell();
+  birthYear.textContent = somePerson.birth_year;
+
+  let height = row.insertCell();
+  height.textContent = somePerson.height;
+
+  let mass = row.insertCell();
+  mass.textContent = somePerson.mass;
+}
+
+function writePlanetsInTable(list) {
+  list.forEach(planets => {
+    insertRowContentForPlanet(planets);
+  });
+}
+
+function insertRowContentForPlanet(somePlanet) {
+
+  let row = tBodyPlanets.insertRow();
+
+  let name = row.insertCell();
+  name.textContent = somePlanet.name;
+
+  let diameter = row.insertCell();
+  diameter.textContent = somePlanet.diameter;
+
+  let climate = row.insertCell();
+  climate.textContent = somePlanet.climate;
+
+  let terrain = row.insertCell();
+  terrain.textContent = somePlanet.terrain;
+
+  let rotationPeriod = row.insertCell();
+  rotationPeriod.textContent = somePlanet.rotation_period;
+
+  let population = row.insertCell();
+  population.textContent = somePlanet.population;
+
+}
+
+function hideDomElements() {
+
+  table1People.style.display = "none";
+  table2Planets.style.display = "none";
+  logoPicture.style.display = "none";
+  searchButton.style.display = "none";
+  inputSearch.style.display = "none";
+}
+
+function callPeople() {
+
+  // Sending request for star wars people
+  function sendRequestForPeople(urlPeople) {
+
+    disableLink(peopleButton);
+    fetch(urlPeople)
+      .then(response => response.json())
+      .then(function (myJson) {
+
+        for (const person of myJson.results) {
+          people.push(person);
+        }
+
+        /* one request is returning only 10 planets 
+        so we are using recursion to get the rest of the elements */
+        if (myJson.next) { sendRequestForPeople(myJson.next); }
+
+        // To hide the animation
+        else {
+          animationGears.style.display = "none";
+          writePeopleInTable(people);
+          table1People.style.display = "";
+          enableLink(peopleButton);
+        }
+      });
   }
-
-  function insertRowContentForPerson(somePerson) {
-
-    let row = tBodyPeople.insertRow();
-
-    let name = row.insertCell();
-    name.textContent = somePerson.name;
-
-    let gender = row.insertCell();
-    gender.textContent = somePerson.gender;
-
-    let birthYear = row.insertCell();
-    birthYear.textContent = somePerson.birth_year;
-
-    let height = row.insertCell();
-    height.textContent = somePerson.height;
-
-    let mass = row.insertCell();
-    mass.textContent = somePerson.mass;
+  //to show the animation while waiting
+  animationGears.style.display = "";
+  hideDomElements();
+  searchButton.style.display = "";
+  inputSearch.style.display = "";
+  if (people.length === 0) { sendRequestForPeople(peopleUrl); }
+  else {
+    animationGears.style.display = "none";
+    table1People.style.display = "";
   }
+}
 
-  function writePlanetsInTable(someArray) {
-    someArray.forEach(pageOfplanets => {
-      let listOfplanets = pageOfplanets.results;
-      listOfplanets.forEach(planets => {
-        insertRowContentForPlanet(planets);
-      })
-    });
+function callPlanets() {
+
+  // Sending request for star wars planets
+  function sendRequestForPlanets(urlPeople) {
+
+    disableLink(planetsButton);
+    fetch(urlPeople)
+      .then(response => response.json())
+      .then(function (myJson) {
+
+        for (const planet of myJson.results) {
+          planets.push(planet);
+        }
+
+        /* one request is returning only 10 planets 
+        so we are using recursion to get the rest of the elements */
+        if (myJson.next) { sendRequestForPlanets(myJson.next); }
+
+        // To hide the animation and enable the link
+        else {
+          animationGears.style.display = "none";
+          enableLink(planetsButton);
+          writePlanetsInTable(planets);
+          table2Planets.style.display = "";
+        }
+      });
   }
-
-  function insertRowContentForPlanet(somePlanet) {
-
-    let row = tBodyPlanets.insertRow();
-
-    let name = row.insertCell();
-    name.textContent = somePlanet.name;
-
-    let diameter = row.insertCell();
-    diameter.textContent = somePlanet.diameter;
-
-    let climate = row.insertCell();
-    climate.textContent = somePlanet.climate;
-
-    let terrain = row.insertCell();
-    terrain.textContent = somePlanet.terrain;
-
-    let rotationPeriod = row.insertCell();
-    rotationPeriod.textContent = somePlanet.rotation_period;
-
-    let population = row.insertCell();
-    population.textContent = somePlanet.population;
-
+  //to show the animation while waiting
+  animationGears.style.display = "";
+  hideDomElements();
+  searchButton.style.display = "";
+  inputSearch.style.display = "";
+  if (planets.length === 0) {
+    sendRequestForPlanets(planetsUrl);
   }
-
-  function hideDomElements() {
-    searchButton.style.display = "none";
-    inputSearch.style.display = "none";
-    table1People.style.display = "none";
-    table2Planets.style.display = "none";
-    logoPicture.style.display = "none";
-
+  else {
+    animationGears.style.display = "none";
+    table2Planets.style.display = "";
   }
+}
 
-  function disableLinks () {
-    peopleButton.classList.add("inactiveLink");
-    planetsButton.classList.add("inactiveLink");
-    homeButton.classList.add("inactiveLink");
-  }
+function callHome() {
+  hideDomElements();
+  logoPicture.style.display = "";
+}
 
-  function enableLinks () {
-    peopleButton.classList.remove("inactiveLink");
-    planetsButton.classList.remove("inactiveLink");
-    homeButton.classList.remove("inactiveLink");
-  }
+function disableLink(button) {
+  button.classList.add("inactiveLink");
+}
 
-  function callPeople() {
+function enableLink(button) {
+  button.classList.remove("inactiveLink");
+}
 
-    // Sending request for star wars people
-    function sendRequestForPeople(urlPeople) {
+// assigning all DOM elements that we need in a variable
+let homeButton = document.getElementById("goHome");
+let peopleButton = document.getElementById("getPeople");
+let planetsButton = document.getElementById("getPlanets");
+let searchButton = document.getElementById("search");
+let inputSearch = document.querySelector("input.form-control");
+let tBodyPeople = document.getElementById("bodyPeople");
+let tBodyPlanets = document.getElementById("bodyPlanets");
+let table1People = document.getElementById("table1");
+let table2Planets = document.getElementById("table2");
+let animationGears = document.getElementById("gears");
+let logoPicture = document.getElementById("logo");
 
-      fetch(urlPeople)
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (myJson) {
+peopleButton.addEventListener("click", callPeople);
+planetsButton.addEventListener("click", callPlanets);
+homeButton.addEventListener("click", callHome);
 
-          //disabeling the link to prevent sending another request
-          disableLinks ();
+// })();
 
-          //to show the animation while waiting
-          animationGears.style.display = "";
+function searchResults(list, string) {
 
-          people.push(myJson);
+  list.forEach(element => {
 
-          /* one request is returning only 10 planets 
-          so we are using recursion to get the rest of the elements */
-          if (myJson.next) { sendRequestForPeople(myJson.next); }
+  });
 
-          // To hide the animation and enable the link
-          else {
-            animationGears.style.display = "none";
-            enableLinks ();
-            writePeopleInTable(people);
-            table1People.style.display = "";
-          }
-        });
-    }
+}
 
-    hideDomElements();
-    if (people.length === 0) { sendRequestForPeople(peopleUrl); }
-    else { table1People.style.display = ""; }
-  }
+function mapArray() {
 
-  function callPlanets() {
 
-    // Sending request for star wars planets
-    function sendRequestForPlanets(urlPeople) {
-
-      fetch(urlPeople)
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (myJson) {
-
-          //disabeling the link to prevent sending another request
-          disableLinks ();
-
-          //to show the animation while waiting
-          animationGears.style.display = "";
-
-          planets.push(myJson);
-
-          /* one request is returning only 10 planets 
-          so we are using recursion to get the rest of the elements */
-          if (myJson.next) { sendRequestForPlanets(myJson.next); }
-
-          // To hide the animation and enable the link
-          else {
-            animationGears.style.display = "none";
-            enableLinks ();
-            writePlanetsInTable(planets);
-            table2Planets.style.display = "";
-          }
-        });
-    }
-
-    hideDomElements();
-    if (planets.length === 0) { sendRequestForPlanets(planetsUrl); }
-    else { table2Planets.style.display = ""; }
-  }
-
-  function callHome() {
-    hideDomElements();
-    inputSearch.style.display = "";
-    searchButton.style.display = "";
-    logoPicture.style.display = "";
-  }
-
-  // assigning all DOM elements that we need in a variable
-  let homeButton = document.getElementById("goHome");
-  let peopleButton = document.getElementById("getPeople");
-  let planetsButton = document.getElementById("getPlanets");
-  let searchButton = document.getElementById("search");
-  let inputSearch = document.querySelector("input.form-control");
-  let tBodyPeople = document.getElementById("bodyPeople");
-  let tBodyPlanets = document.getElementById("bodyPlanets");
-  let table1People = document.getElementById("table1");
-  let table2Planets = document.getElementById("table2");
-  let animationGears = document.getElementById("gears");
-  let logoPicture = document.getElementById("logo");
-
-  peopleButton.addEventListener("click", callPeople);
-  planetsButton.addEventListener("click", callPlanets);
-  homeButton.addEventListener("click", callHome);
-
-})();
-
+}
